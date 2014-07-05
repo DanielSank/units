0d2cd79d2ec8e1801913e46bb8e0a899275cb48c
Daniel Sank <sank.daniel@gmail.com>
2014-07-05 07:44:44 -0700

    Working tag parsing

--- units/units.py
+++ units/units.py
@@ -1,3 +1,4 @@
+import fractions
 import re
 
 SI_POWERS = { #prefix -> power of ten
@@ -10,46 +11,37 @@ SI_POWERS = { #prefix -> power of ten
 
 MULT = '*'
 
-class Unit(object):
-    """
-    A physical unit.
-    
-    Basically a set of basic units with powers.
-    """
-    def __init__(self, tag):
-        d = self.parse_tag(tag)
-        self._data = d
-    
-    @staticmethod
-    def parse_tag(tag):
-        """Returns a map from unit->power"""
-        multiplied_tags = tag.split(MULT)
-        divided_tags = []
-        for multiplied_tag in multiplied_tags:
-            # Find each division by another unit
-            # while ignoring division in powers.
-            pattern = re.compile(r"/[^0-9]") #Divide by not a number
-            matches = re.finditer(pattern, multiplied_tag)
-            for m in matches:
-                start, end = m.span()
-                divided_tags.append(multiplied_tag[start+1:end])
-        return multiplied_tags, divided_tags
-    
-    def __str__(self):
-        pass
 
-def powers(s):
-    """Returns positive and negative parts of a rational power"""
-    if '/' in s:
-        pos, neg = (int(n) for n in s.split('/'))
-        if pos == 1:
-            pos = 0
-    else:
-        pos, neg = (int(s), 0)
-    return pos, neg
+class Unit(object):
+    pass
+
+
+def parse_tag(tag):
+    """Returns a map from letter -> power"""
+    parsed = {}
+    numerator, denominator = numerator_denominator(tag)
+    for simple_tags, factor in zip((numerator, denominator), (1, -1)):
+        for t in simple_tags:
+            letter, power = parse_one_dimensional_tag(t)
+            power = power * factor
+            existing_power = parsed.get(letter, 0)
+            parsed[letter] = power + existing_power
+    return parsed
+
 
 def numerator_denominator(tag):
-    """Returns the numerator and denominator parts of a tag"""
+    """
+    Returns the numerator and denominator parts of a tag
+    
+    For example
+    >>> numerator_denominator('s*m^2/Hz/K^3/4*L/s')
+    >>> (['s', 'm^2', 'L'], ['Hz', 'K^3/4', s'])
+    
+    The order of the elements within the numerator and denominator lists is
+    not specified.
+    
+    Note that a tag can appear both in the numerator and denominator.
+    """
     numerator = []
     denominator = []
     for part in tag.split(MULT):
@@ -62,4 +54,28 @@ def numerator_denominator(tag):
         numerator.append(slash_separated.pop(0))
         denominator.extend([den for den in slash_separated])
     return numerator, denominator
+
+
+def parse_one_dimensional_tag(tag):
+    """
+    Returns (letter, power) for a single dimensional tag.
+    
+    A simple tag must have one of the following forms:
+        [a-zA-Z]+
+        [a-zA-Z]+^n
+        [a-zA-Z]+^n/m
+    
+    Example:
+    >>> parse_single_tag('K^3/4')
+    >>> ('K', 3, 4)
+    """
+    # XXX check form of tag (ie using the regexp in the doc string)
+    parts = tag.split('^')
+    if len(parts) == 1:
+        letter = parts[0]
+        power = fractions.Fraction(1)
+    elif len(parts) == 2:
+        letter, exponent = parts
+        power = fractions.Fraction(parts[1])
+    return letter, power
 
